9ec88b955c24aff1152a89b380f5a77871f0addf
JemalleDash <95877914+JemalleDash@users.noreply.github.com>
2022-06-21 23:48:05 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,128 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "Лаб Электроэнергетики (с номерами)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Лаб Электроэнергетики",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.33053254161221446,
+          "pitch": -0.0199451352370712,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Комплекс типового лабораторного оборудования\n«Распределение сети систем электроснабжения»</span>"
+        },
+        {
+          "yaw": 0.7473293271660975,
+          "pitch": 0.002739051759558464,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Лабораторный стенд ЭИ-02 «Электротехнические\nизмерения»</span>"
+        },
+        {
+          "yaw": 1.2067529628041687,
+          "pitch": 0.025148429317368937,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Лабораторная установка «Электрический привод ЭП-1»</span>"
+        },
+        {
+          "yaw": 1.041388743428607,
+          "pitch": 0.3397877464952366,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\"><a href=\"https://www.youtube.com/watch?v=PNa9MF1DoOE\">Нажмите для просмотра видео</a><o:p></o:p></span></b></p>",
+          "text": ""
+        },
+        {
+          "yaw": 1.7724274234165351,
+          "pitch": -0.010681221217547332,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Лабораторный стенд «Качество электрической энергии\nв системах энергоснабжения»</span>"
+        },
+        {
+          "yaw": 1.9933833309004774,
+          "pitch": -0.0066927928852180685,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<p class=\"MsoNormal\" style=\"text-align:justify;text-justify:inter-ideograph\"><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Лабораторный\nстенд НТЦ-10.000 «Электроснабжение промышленных предприятий»<o:p></o:p></span></p>"
+        },
+        {
+          "yaw": 2.253010245696901,
+          "pitch": -0.003928367369699259,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Лабораторный стенд НТЦ-08.15 «Монтаж и наладка\nэлектрооборудования предприятий и гражданских зданий»</span>"
+        },
+        {
+          "yaw": 2.4794065398240157,
+          "pitch": -0.006330417290010715,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<p class=\"MsoNormal\" style=\"text-align:justify;text-justify:inter-ideograph\"><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Лабораторный\nстенд НТЦ-01.06 «Теоретические основы электротехники»<o:p></o:p></span></p>"
+        },
+        {
+          "yaw": 2.9136523087026553,
+          "pitch": 0.1932596224593972,
+          "title": "<p class=\"MsoNormal\"><b><span style=\"font-size:14.0pt;line-height:115%;font-family:&quot;Times New Roman&quot;,serif\">Нажмите\nдля просмотра информации<o:p></o:p></span></b></p>",
+          "text": "<span style=\"font-size:14.0pt;line-height:115%;\nfont-family:&quot;Times New Roman&quot;,serif;mso-fareast-font-family:&quot;Times New Roman&quot;;\nmso-fareast-theme-font:minor-fareast;mso-ansi-language:RU;mso-fareast-language:\nRU;mso-bidi-language:AR-SA\">Аппарат\nиспытания масла АИМ-90И</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
